a259fae27657e7fbf75b6dbe806cfb1e491ba60f
CasperLCasper <64712311+CasperLCasper@users.noreply.github.com>
2026-08-10 18:50:26 +0300

    Update config.js

--- cli/src/config.js
+++ cli/src/config.js
@@ -27,7 +27,7 @@ const CONFIG = {
     // ==========================================
     // WEB URL
     // ==========================================
-    WEB_URL: 'https://permrepo.pages.dev',
+    WEB_URL: 'https://permrepo-topup-1.onrender.com',
     NFT_PAGE: '/nft.html',
     SUBSCRIBE_PAGE: '/subscribe.html',
     STORAGE_PAY_PAGE: '/storage-pay.html',
